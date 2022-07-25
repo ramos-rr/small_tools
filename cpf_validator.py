59ac41bab41308694cfb80f9acaffc63129b2523
ramos-rr <ramos-rr@outlook.com>
2022-07-25 10:40:41 -0300

    CPF validator updated

--- cpf_validator.py
+++ cpf_validator.py
@@ -1,18 +1,34 @@
-'''
-Módulo para a verificação de CPF
-1. Considera apenas os números, ignorando os pontos e traços
-2. Realiza a contagem de caracteres e retorna Falso se não contiver 11 números
-3. Realiza a verificação do dígito, retornando Falso caso esteja errado.
-4. Retorna verdadeiro se o CPF for digitado corretamente.
+# '''
+# Módulo para a verificação de CPF
+# 1. Considera apenas os números, ignorando os pontos e traços
+# 2. Realiza a contagem de caracteres e retorna Falso se não contiver 11 números
+# 3. Realiza a verificação do dígito, retornando Falso caso esteja errado.
+# 4. Retorna verdadeiro se o CPF for digitado corretamente.
+#
+#     >>> cpf = "325.171.828-25"
+#     True
+#     >>> cpf = 32517182826
+#     False
+# '''
 
-    >>> cpf = "325.171.828-25"
-    True
-    >>> cpf = 32517182826
+
+def verifique_cpf(cpf) -> bool or int:
+    """
+    Função que retorna se o CPF digitado é válido. O usuário pode entrar com o CPF juntamente com os pontos e traços
+    que a função somente irá considerar os números:
+    Ex:
+    >>> verifique_cpf('325.171.828-25')
+    '32517182825'
+    >>> verifique_cpf('385259448-07')
+    '38525944807'
+    >>> verifique_cpf('32517182825')
+    '32517182825'
+    >>> verifique_cpf('32517182829')
     False
-'''
 
-
-def verifique_cpf(cpf) -> bool:
+    :param cpf: Conjunto de caracteres numéricos que formam o CPF.
+    :return: True se o CPF digitado for válido. False se o CPF digitado não for válido
+    """
     cpf = str(cpf)
     cpf_list = [n for n in cpf if n in '0123456789']
     if len(cpf_list) != 11:
@@ -41,10 +57,5 @@ def verifique_cpf(cpf) -> bool:
                 digito_dois = 0
             if digito_dois != int(cpf_list[-1]):
                 return False
-    return True
-
-
-if __name__ == '__main__':
-    cpf = verifique_cpf(input('digite o cpf: '))
-    if not cpf:
-        print('CPF inválido')
+    cpf = ''.join(cpf_list)
+    return cpf
